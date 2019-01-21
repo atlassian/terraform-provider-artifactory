c185569d1024dba72812c1d92f207b4f788ff7f1
Dillon Giacoppo <dgiacoppo@atlassian.com>
2019-01-21 13:59:33 +1100

    made run posix compliant

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env sh
 
+set -euf
 docker run -i -t -d --rm -v "${PWD}/scripts/artifactory.lic:/artifactory_extra_conf/artifactory.lic:ro" -p8080:8081 docker.bintray.io/jfrog/artifactory-pro:6.6.5
 
-CMD="curl -s --max-time 5 -o /dev/null -w %{http_code} --fail http://localhost:8080/artifactory/webapp/#/login"
-
 echo "Waiting for Artifactory to start"
-while [[ ! "$($CMD)" =~ ^(2|4)[0-9]{2} ]]; do
-    echo "."
+until curl --output /dev/null --silent --head --fail http://localhost:8080/artifactory/webapp/#/login; do
+    echo '.'
     sleep 4
 done
 
 # Use decrypted passwords
-curl -u admin:password localhost:8080/artifactory/api/system/decrypt -X POST
+curl -u admin:password  --output /dev/null --silent --fail localhost:8080/artifactory/api/system/decrypt -X POST
